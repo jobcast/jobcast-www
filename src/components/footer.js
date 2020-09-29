36a11255d48beeb35f7a3aeb61accf7b6a207229
Johnny Oshika <johnnyoshika@gmail.com>
2020-09-29 16:25:04 -0700

    Populate header and footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,7 +1,110 @@
 import React from 'react'
+import socialLinkedIn from '../assets/images/socialLinkedIn.svg'
+import socialTwitter from '../assets/images/socialTwitter.svg'
+import socialFacebook from '../assets/images/socialFacebook.svg'
+import socialYouTube from '../assets/images/socialYouTube.svg'
 
 const Footer = () => {
-  return <div>Footer</div>
+  return (
+    <footer className="footer clear xs-ph15" role="contentinfo">
+      {/* use this instead of home page <footer className="footer clear margin-0 xs-p15" role="contentinfo"> */}
+      <div className="wrapper">
+        <div className="footer-social">
+          <ul className="margin-0">
+            <li>
+              <a
+                rel="noreferrer"
+                href="https://www.linkedin.com/company/jobcast-net"
+                target="_blank"
+                title="Jobcast on LinkedIn"
+              >
+                <img
+                  src={socialLinkedIn}
+                  alt="LinkedIn"
+                  height="21"
+                  width="21"
+                />
+              </a>
+            </li>
+            <li>
+              <a
+                rel="noreferrer"
+                href="https://twitter.com/jobcastnet"
+                target="_blank"
+                title="Jobcast on Twitter"
+              >
+                <img
+                  src={socialTwitter}
+                  alt="Twitter"
+                  height="21"
+                  width="21"
+                />
+              </a>
+            </li>
+            <li>
+              <a
+                rel="noreferrer"
+                href="https://www.facebook.com/jobcastnet"
+                target="_blank"
+                title="Jobcast on Facebook"
+              >
+                <img
+                  src={socialFacebook}
+                  alt="Facebook"
+                  height="21"
+                  width="21"
+                />
+              </a>
+            </li>
+            <li>
+              <a
+                rel="noreferrer"
+                href="https://www.youtube.com/channel/UCHov9o2-HsiPV2Awp2ke-TA"
+                target="_blank"
+                title="Jobcast on Youtube"
+              >
+                <img
+                  src={socialYouTube}
+                  alt="YouTube"
+                  height="21"
+                  width="21"
+                />
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="footer-nav">
+          <ul>
+            <li>
+              <a href="http://www.jobcast.net/about/">About</a>
+            </li>
+            <li>
+              <a href="http://www.jobcast.net/contact-us/">Contact</a>
+            </li>
+            <li>
+              <a href="http://www.jobcast.net/jobcast-faq/">FAQ</a>
+            </li>
+            <li>
+              <a href="http://www.jobcast.net/terms/">Terms</a>
+            </li>
+            <li>
+              <a href="http://www.jobcast.net/privacy/">Privacy</a>
+            </li>
+            <li>
+              <a href="https://app.jobcast.net/developer">API</a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="footer-top pull-right text-center">
+          <a href="#topline" className="pointer">
+            Back to top
+          </a>
+        </div>
+      </div>
+    </footer>
+  )
 }
 
 export default Footer
--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,92 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'gatsby'
+import logo from '../assets/images/logo.png'
 
 const Header = () => {
-  return <div>Header</div>
+  const [navActive, setNavActive] = useState(false)
+
+  return (
+    <header className="header clear" role="banner" id="topline">
+      {/* use this instead for home page <header className="header clear margin-0" role="banner" id="topline"> */}
+      <div className="wrapper">
+        <div className="logo">
+          <Link to="/">
+            <img src={logo} alt="Jobcast Logo" className="logo-img" />
+          </Link>
+        </div>
+        <div className="hidden-xs pull-right nav-login-wrapper">
+          <a
+            href="https://app.jobcast.net/dashboard/"
+            title="Log in to Jobcast"
+            className="nav-login btn btn-second btn-small"
+          >
+            Log in
+          </a>
+        </div>
+        <div
+          className={`nav-toggle ${navActive ? 'active' : ''}`}
+          onClick={() => setNavActive(!navActive)}
+          onKeyDown={() => setNavActive(!navActive)}
+          role="button"
+          tabIndex="0"
+        >
+          <div
+            className={`nav-toggle-inner ${
+              navActive ? 'active' : ''
+            }`}
+          ></div>
+        </div>
+        <nav
+          className={`nav ${navActive ? 'active' : ''}`}
+          role="navigation"
+        >
+          <ul>
+            <li className="visible-xs">
+              <a href="https://app.jobcast.net/dashboard/">Log In</a>
+            </li>
+            <li>
+              <a href="http://www.jobcast.net/plans/">Pricing</a>
+            </li>
+            <li>
+              <a href="http://www.jobcast.net/job-sync/">Job Sync</a>
+            </li>
+            <li>
+              <a href="http://www.jobcast.net/blog/">Blog</a>
+            </li>
+            <li>
+              <a href="http://www.jobcast.net/jobcast-tutorials/">
+                Tutorials
+              </a>
+            </li>
+            <li>
+              <a href="http://www.jobcast.net/category/resources/">
+                Resources
+              </a>
+            </li>
+            <li className="visible-xs">
+              <a href="http://www.jobcast.net/about/">About</a>
+            </li>
+            <li className="visible-xs">
+              <a href="http://www.jobcast.net/contact-us/">Contact</a>
+            </li>
+            <li className="visible-xs">
+              <a href="http://www.jobcast.net/jobcast-faq/">FAQ</a>
+            </li>
+
+            <li className="visible-xs">
+              <a href="http://www.jobcast.net/terms/">Terms</a>
+            </li>
+            <li className="visible-xs">
+              <a href="http://www.jobcast.net/privacy/">Privacy</a>
+            </li>
+            <li className="visible-xs">
+              <a href="https://app.jobcast.net/developer">API</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  )
 }
 
 export default Header
