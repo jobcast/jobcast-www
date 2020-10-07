e860a4d8dcad1bbb09fa1ff8e10fd1e86894fdbb
Johnny Oshika <johnnyoshika@gmail.com>
2020-10-07 12:33:14 -0700

    Add page title

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,10 +1,32 @@
 import React from 'react'
+import { graphql, useStaticQuery } from 'gatsby'
+import { Helmet } from 'react-helmet'
 import Full from './full'
 import Fixed from './fixed'
 import './styles.css'
 
 const Layout = props => {
-  return props.full ? <Full {...props} /> : <Fixed {...props} />
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
+
+  return (
+    <>
+      <Helmet>
+        <title>
+          {props.title ? props.title + ' |' : ''}{' '}
+          {data.site.siteMetadata.title}
+        </title>
+      </Helmet>
+      {props.full ? <Full {...props} /> : <Fixed {...props} />}
+    </>
+  )
 }
 
 export default Layout
