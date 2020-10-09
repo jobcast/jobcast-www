593c0d76e3baa095da58d44d25ca33c6ebf6dbfa
Johnny Oshika <johnnyoshika@gmail.com>
2020-10-08 22:27:19 -0700

    Show list of blog posts

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,8 +1,42 @@
 import React from 'react'
+import { Link, graphql, useStaticQuery } from 'gatsby'
 import Layout from '../../components/layout'
 
 const BlogPage = () => {
-  return <Layout title="Blog"></Layout>
+  const data = useStaticQuery(graphql`
+    query {
+      allContentfulBlogPost(
+        sort: { fields: publishDate, order: DESC }
+      ) {
+        edges {
+          node {
+            slug
+            title
+            publishDate(formatString: "MMMM D")
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <Layout title="Blog">
+      {data.allContentfulBlogPost.edges.map(({ node }) => (
+        <div className="clear margin-b15">
+          <article>
+            <div className="cate-title">
+              <h2>
+                <Link to={`/${node.slug}/`} title={node.title}>
+                  {node.title}
+                </Link>
+              </h2>
+            </div>
+            <div className="cate-date">{node.publishDate}</div>
+          </article>
+        </div>
+      ))}
+    </Layout>
+  )
 }
 
 export default BlogPage
