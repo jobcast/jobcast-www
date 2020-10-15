caea80469105e90819239e4bc44de145d899c86a
Johnny Oshika <johnnyoshika@gmail.com>
2020-10-15 10:16:32 -0700

    Enhance paginator

--- src/components/paginator/index.js
+++ src/components/paginator/index.js
@@ -1,29 +1,96 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
-import styles from './styles.module.css'
+import './styles.css'
 
 const Paginator = ({ basePath, currentPage, totalPages }) => {
-  const isFirst = currentPage === 1
-  const isLast = currentPage === totalPages
-  const prevPage =
-    currentPage - 1 === 1
-      ? basePath
-      : `${basePath}page/${currentPage - 1}/`
-  const nextPage = `${basePath}page/${currentPage + 1}/`
+  const linkTo = page => {
+    if (page < 1) page = 1
+    if (page > totalPages) page = totalPages
+    return page === 1 ? basePath : `${basePath}page/${page}/`
+  }
+
+  const pages = []
+
+  pages.push({
+    link: linkTo(currentPage - 1),
+    label: '«',
+    className: currentPage === 1 ? 'disabled' : '',
+  })
+
+  if (totalPages < 8)
+    Array.from({ length: totalPages }).forEach((_, i) => {
+      const page = i + 1
+      pages.push({
+        link: linkTo(page),
+        label: page,
+        className: currentPage === page ? 'active disabled' : '',
+      })
+    })
+  else {
+    if (currentPage > 5) {
+      pages.push({
+        link: linkTo(1),
+        label: 1,
+        className: 'pagination-d-none pagination-d-md-block',
+      })
+      pages.push({
+        link: linkTo(1),
+        label: '…',
+        className: 'disabled pagination-d-none pagination-d-md-block',
+      })
+    }
+
+    const startRange = Math.min(
+      currentPage < 6 ? 1 : currentPage - 2,
+      totalPages - 4
+    )
+    const endRange = Math.max(
+      currentPage + 5 > totalPages ? totalPages : currentPage + 2,
+      5
+    )
+    for (let page = startRange; page <= endRange; page++)
+      pages.push({
+        link: linkTo(page),
+        label: page,
+        className:
+          page < currentPage - 2 || page > currentPage + 2
+            ? 'pagination-d-none pagination-d-md-block'
+            : currentPage === page
+            ? 'active disabled'
+            : '',
+      })
+
+    if (currentPage < totalPages - 4) {
+      pages.push({
+        link: linkTo(totalPages),
+        label: '…',
+        className: 'disabled pagination-d-none pagination-d-md-block',
+      })
+      pages.push({
+        link: linkTo(totalPages),
+        label: totalPages,
+        className: 'pagination-d-none pagination-d-md-block',
+      })
+    }
+  }
+
+  pages.push({
+    link: linkTo(currentPage + 1),
+    label: '»',
+    className: currentPage === totalPages ? 'disabled' : '',
+  })
+
   return (
-    <div className={styles.paginator}>
-      {!isLast && (
-        <Link className={styles.next} to={nextPage} rel="next">
-          Next Page →
-        </Link>
-      )}
-      {!isFirst && (
-        <Link to={prevPage} rel="prev">
-          ← Previous Page
-        </Link>
-      )}
-    </div>
+    <ul className="pagination pagination-lg pagination-justify-content-center">
+      {pages.map(p => (
+        <li className={`page-item ${p.className}`}>
+          <Link to={p.link} className="page-link">
+            {p.label}
+          </Link>
+        </li>
+      ))}
+    </ul>
   )
 }
 
