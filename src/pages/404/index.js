7f7ab77f8471fc1033ec59fff7db0d81dc8fac2a
Johnny Oshika <johnnyoshika@gmail.com>
2020-10-16 15:39:17 -0700

    Add 404 page

---
+++ src/pages/404/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Link } from 'gatsby'
+import Layout from '../../components/layout'
+import brokenRobot from './images/broken-robot.png'
+
+import styles from './styles.module.css'
+
+const NotFoundPage = () => {
+  return (
+    <Layout>
+      <div className={`clear ${styles.container}`}>
+        <div className="col-xs-12 col-sm-6 text-center">
+          <div className={styles.heading}>Oops...</div>
+          <div className={styles.label}>Page not found!</div>
+          <p className={styles.description}>
+            <Link to="/">Head home</Link>
+          </p>
+        </div>
+        <div className="col-xs-12 col-sm-6 text-center">
+          <img src={brokenRobot} alt="Broken Robot" />
+        </div>
+      </div>
+    </Layout>
+  )
+}
+
+export default NotFoundPage
