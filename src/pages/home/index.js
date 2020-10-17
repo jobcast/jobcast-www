fbf335ef244c852969009b77a42c5e075ab0815c
Johnny Oshika <johnnyoshika@gmail.com>
2020-10-16 18:21:24 -0700

    Fix home page camelCase properties

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -55,9 +55,9 @@ const IndexPage = () => {
               width="320"
               height="240"
               src="https://www.youtube.com/embed/9qFIOUpXst8"
-              frameborder="0"
+              frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowfullscreen
+              allowFullScreen
             ></iframe>
           </div>
 
